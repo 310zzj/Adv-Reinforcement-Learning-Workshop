57921f7608ae8e861bba14b01cd3ebdbdebf5a25
310zzj <310zzj@proton.me>
2024-03-20 06:01:27 +0000

    Edited readme.

--- Workshop_facilitation/generate-groups.py
+++ Workshop_facilitation/generate-groups.py
@@ -9,4 +9,14 @@ import shutil
 import pandas as pd
 
 TMP_FOLDER = 'LINK_TEMP'
-IP_DICT_J
+IP_DICT_JSON_NAME = 'active_ips.json'
+GROUP_DICT_NAME = 'groups.json'
+GROUP_EXCEL_NAME = 'Groups.xlsx'
+AMI_ID = 'ami-060e29600c7769bc0'
+
+
+def bucket_exists(bucket_name):
+    """Determine whether bucket_name exists and the user has permission to access it
+
+    :param bucket_name: string
+    :retu
